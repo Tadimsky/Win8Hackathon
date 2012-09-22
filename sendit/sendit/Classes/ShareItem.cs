2db7bdcdc456ddf68eed13cb85518805e9ffda05
jo63 <jo63@duke.edu>
2012-09-22 00:37:20 -0400

    shareitem

--- sendit/sendit/Classes/ShareItem.cs
+++ sendit/sendit/Classes/ShareItem.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.ApplicationModel.DataTransfer.ShareTarget;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
 
 namespace sendit.Classes
 {
     class ShareItem
     {
+        [DataMember(Name = "Title")]
+        public string text { get; set; }
+
+        [DataMember(Name = "Uri")]
+        public string Uri  { get; set; }
+
+        [DataMember(Name = "Html")]
+        public string html { get; set; }
+
+        [DataMember(Name = "Bitmap")]
+        public BitmapImage bitmap { get; set; }
+
+        [DataMember(Name = "File")]
+        public StorageFile file { get; set; }
 
     }
 }
